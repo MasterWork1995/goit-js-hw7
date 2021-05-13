14675e199455854356bad5095820c6843affb0f6
Yurii Yurents <yura19950117@gmail.com>
2021-05-13 15:48:43 +0300

    task 2 done

---
+++ src/js/task-02.js
@@ -0,0 +1,23 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+    const ingredientsItemEl = document.createElement('li');
+    ingredientsItemEl.classList.add('ingredients-item');
+    const itemTextEl = document.createElement('p');
+    itemTextEl.classList.add('ingredients-text');
+    itemTextEl.textContent = option;
+    ingredientsItemEl.appendChild(itemTextEl);
+    return ingredientsItemEl;
+}); 
+
+ingredientsRef.append(...elements);
+console.log(ingredientsRef);
