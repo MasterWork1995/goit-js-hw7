b8c67db6dd8c880c3a97cfc52905c9a9b64efaaf
Yurii Yurents <yura19950117@gmail.com>
2021-05-13 16:15:31 +0300

    update task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,7 +9,19 @@ const ingredients = [
 
 const ingredientsRef = document.querySelector('#ingredients');
 
-const elements = ingredients.map(option => {
+// const elements = ingredients.map(option => {
+//     const ingredientsItemEl = document.createElement('li');
+//     ingredientsItemEl.classList.add('ingredients-item');
+//     const itemTextEl = document.createElement('p');
+//     itemTextEl.classList.add('ingredients-text');
+//     itemTextEl.textContent = option;
+//     ingredientsItemEl.appendChild(itemTextEl);
+//     return ingredientsItemEl;
+// }); 
+// ingredientsRef.append(...elements);
+
+const ingredientsOptions = (options) => {
+  return options.map(option => {
     const ingredientsItemEl = document.createElement('li');
     ingredientsItemEl.classList.add('ingredients-item');
     const itemTextEl = document.createElement('p');
@@ -17,7 +29,11 @@ const elements = ingredients.map(option => {
     itemTextEl.textContent = option;
     ingredientsItemEl.appendChild(itemTextEl);
     return ingredientsItemEl;
-}); 
+  }); 
+};
 
+const elements = ingredientsOptions(ingredients);
 ingredientsRef.append(...elements);
+
 console.log(ingredientsRef);
+
