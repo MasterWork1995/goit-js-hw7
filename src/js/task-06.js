5f830943fadc6eec7976a8a3ee17b98b6f68f86c
Yurii Yurents <yura19950117@gmail.com>
2021-05-19 18:58:22 +0300

    task 6 done

---
+++ src/js/task-06.js
@@ -0,0 +1,14 @@
+const inputChange = document.querySelector('#validation-input');
+
+inputChange.addEventListener('blur', onInputLength);
+
+function onInputLength (event) {
+    if (event.currentTarget.value.length >= inputChange.dataset.length) {
+        inputChange.classList.add('valid');
+        inputChange.classList.remove('invalid');
+    } else {
+        inputChange.classList.add('invalid');
+        inputChange.classList.remove('valid');
+    }
+};
+
